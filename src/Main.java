53cc7d6437d656dcb800d8691091372ccbaf524e
Liscak Adam <1810653053@stud.fh-kufstein.ac.at>
2018-10-11 11:23:15 +0200

    uiguu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args)  { String nnm = "van Kokgit" ; int alter= 1998;
+    public static void main(String[] args)  { string vnm = "Rolo";  String nnm = "van Kokgit" ; int alter= 1998;
         System.out.println("Hello "+nnm+" du bist im Jahre "+alter+ " geboren");
     }
 }
