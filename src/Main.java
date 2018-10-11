f85080d3d9cf6949ba124fba50b037dff24f3195
Liscak Adam <1810653053@stud.fh-kufstein.ac.at>
2018-10-11 10:51:23 +0200

    gok

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args)  { String vorname = "Werner van Kokgit ig" ; int alter= (int) 34.897;
+        System.out.println("Hello "+vorname+" du bist "+alter);
+    }
+}
