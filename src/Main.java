e87763593e96453968b0b6efbfa892e102dde6a8
Liscak Adam <1810653053@stud.fh-kufstein.ac.at>
2018-10-11 11:27:04 +0200

    Döner kebap update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args)  { string vnm = "Rolo";  String nnm = "van Kokgit" ; int alter= 1998;
-        System.out.println("Hello "+nnm+" du bist im Jahre "+alter+ " geboren");
+    public static void main(String[] args)  { String vnm = "Rolo";  String nnm = "van Kokgit" ; int alter= 1998; int alter2=666
+        System.out.println("Hello "+vnm+" "+nnm+" du bist im Jahre "+alter+ " geboren. Du bist " +alter2+" Jahre alt" );
     }
 }
