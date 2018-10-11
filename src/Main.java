06126666f09b85be56d7061739549760993d1b21
Liscak Adam <1810653053@stud.fh-kufstein.ac.at>
2018-10-11 11:29:46 +0200

    Kurvywurst

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args)  { String vnm = "Rolo";  String nnm = "van Kokgit" ; int alter= 1998; int alter2=666
+    public static void main(String[] args)  { String vnm = "Rolo";  String nnm = "van Kokgit" ; int alter= 1998; int alter2=666;
         System.out.println("Hello "+vnm+" "+nnm+" du bist im Jahre "+alter+ " geboren. Du bist " +alter2+" Jahre alt" );
     }
 }
