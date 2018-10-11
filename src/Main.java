af6fd451a2dba1d1a45a2ade517bcc8ce67a430c
Liscak Adam <1810653053@stud.fh-kufstein.ac.at>
2018-10-11 11:09:02 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args)  { String vorname = "Werner van Kokgit ig" ; int alter= (int) 34.897;
-        System.out.println("Hello "+vorname+" du bist "+alter);
+    public static void main(String[] args)  { String nnm = "van Kokgit" ; int alter= 1998;
+        System.out.println("Hello "+nnm+" du bist im Jahre "+alter+ " geboren");
     }
 }
